ffb9353dac793bfebbaa866215e552af59c887fa
moreniekmeijer <niekjmeijer@gmail.com>
2025-03-10 17:45:06 +0100

    Added basic routing and content for Post and Overview page

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,7 +1,22 @@
+import posts from '../../constants/data.json';
+import {Link} from "react-router-dom";
+import "./Overview.css"
+
 function Overview() {
     return (
-        <>
-        </>
+        <section className="overview">
+            <h1>Bekijk de {posts.length} posts op de pagina</h1>
+            <ul className="post-list">
+                {posts.map(post => {
+                    return (
+                        <li key={post.id} className="post-item">
+                            <h3><Link to={`/post/${post.id}`}>{post.title}</Link> ({post.author})</h3>
+                            <p>{post.comments} reacties - {post.shares} keer gedeeld</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </section>
     )
 }
 
--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -1,11 +1,21 @@
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import posts from "../../constants/data.json";
+import formatDate from "../../helpers/formatDate.js";
 
 function Post() {
     const { id } = useParams();
 
+    const {title, readTime, subtitle, author, created, content} = posts.find((post) => post.id.toString() === id);
+
+    const formattedDate = formatDate(created);
+
     return (
         <>
-            <h2>Post {id}</h2>
+            <h2>{title} ({readTime} minuten)</h2>
+            <h3>{subtitle}</h3>
+            <p>Geschreven door {author} op {formattedDate}</p>
+            <p>{content}</p>
+            <Link to="/alle-posts">Terug naar de overzichtspagina</Link>
         </>
     )
 }
